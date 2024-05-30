38ca97e7fd10f0c19d0af6f1ada6558c4c3df299
lolk186 <bzuev662@gmail.com>
2024-05-31 00:13:44 +0300

    доделаль

--- 3zd/main.js
+++ 3zd/main.js
@@ -1,3 +1,7 @@
 function toggleTheme(){
-    
+    let body = document.getElementById("body")
+    console.log(body)
+    body.removeAttribute("class")
+    body.setAttribute("class","dark-mode")
+    console.log(body)
 }
