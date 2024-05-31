56ef130ceb5d22806ace679ef6e65b2ec8b06fa1
cybersport <bzuev662@gmail.com>
2024-05-31 11:57:53 +0300

    5 задание выполнено

---
+++ 5zd/main.js
@@ -0,0 +1,8 @@
+function changeImage(){
+    let img = document.getElementById("hoverImage")
+    img.setAttribute("src","photo/photo_2024-05-19_20-28-07.jpg")
+}
+function resetImage(){
+    let img = document.getElementById("hoverImage")
+    img.setAttribute("src","photo/photo_2024-05-30_01-18-16.jpg")
+}
