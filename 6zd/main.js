99079b53959efb109584cdf13842203dd2b20c06
cybersport <bzuev662@gmail.com>
2024-05-31 13:05:26 +0300

    3 задание

---
+++ 6zd/main.js
@@ -0,0 +1,10 @@
+var num = 1
+function addOptions(){
+    let select = document.getElementById("dropdown")
+    let addoption = document.createElement("option")
+    select.appendChild(addoption)
+    addoption.setAttribute("id",`${num}option`)
+    let option = document.getElementById(`${num}option`)
+    option.innerHTML=`${num} опция`
+    num++
+}
